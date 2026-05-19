f1f091e07e30a56ef6429d845720e493286705dd
Alex <alexalexbabarika@gmail.com>
2026-05-19 14:58:17 +0100

    feat(hifigan): add support for hifi gan

---
+++ inference/hifigan/config.py
@@ -0,0 +1,15 @@
+V1_CONFIG = {
+    "resblock": "1",
+    "upsample_rates": [8, 8, 2, 2],
+    "upsample_kernel_sizes": [16, 16, 4, 4],
+    "upsample_initial_channel": 512,
+    "resblock_kernel_sizes": [3, 7, 11],
+    "resblock_dilation_sizes": [[1, 3, 5], [1, 3, 5], [1, 3, 5]],
+    "num_mels": 80,
+    "n_fft": 1024,
+    "hop_size": 256,
+    "win_size": 1024,
+    "sampling_rate": 22050,
+    "fmin": 0,
+    "fmax": 8000,
+}
